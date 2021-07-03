d735b0971d3e8453659fbee17c39e28be62b5fc0
devedux <devedux@gmail.com>
2021-07-03 16:16:46 +0000

    Validation in file extension

--- main.rb
+++ main.rb
@@ -3,7 +3,7 @@ require_relative "commons/helper/prompter"
 store = ARGV.shift
 
 def file_extension?(store)
-  return puts("---You need to Enter an file valid---".red) unless !store.nil? && store.match?("json")
+  return puts("---You need to Enter an file valid---".red) unless store.nil? || (!store.nil? && store.match?("json"))
 
   true
 end
